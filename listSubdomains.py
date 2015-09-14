6803f576284fe7a83a51c0fb35b1d28687025bf9
Ciaran McNally <mak@redbrick.dcu.ie>
2015-09-14 19:38:05 -0400

    Combined subdomain lookup

--- listSubdomains.py
+++ listSubdomains.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 from DNSDumpsterAPI import DNSDumpsterAPI
+from NetcraftAPI import NetcraftAPI
 import sys
 
 domain = sys.argv[1]
 res = DNSDumpsterAPI({'verbose': False}).search(domain)
+res2 = NetcraftAPI({'verbose': False}).search(domain)
+
 for i in res:
 	print i+'.'+domain
+
+for i in res2:
+	if i not in res:
+		print i+'.'+domain
+
+
